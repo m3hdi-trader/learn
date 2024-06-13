26081219d688f5290431b2c8281602ff2509f5fc
mehdi <m3hdi.trader@gmail.com>
2024-06-13 10:06:54 +0330

    Refactor user model

--- 22-tdd/app/Models/User.php
+++ 22-tdd/app/Models/User.php
@@ -2,29 +2,46 @@
 
 namespace App\Models;
 
+use App\Helpers;
+
 class User
 {
     protected $first_name;
     protected $last_name;
-    public function setFirstName($fistName)
+
+    public function __call($method, $params)
     {
-        $this->first_name = trim($fistName);
+        $property = Helpers::camelToUnderscore(substr($method, 3));
+
+        if (property_exists($this, $property)) {
+            if (substr($method, 0, 3) == 'set') {
+                $this->$property = trim($params[0]);
+            }
+        }
+        if (substr($method, 0, 3) == 'get') {
+            return $this->$property;
+        }
     }
 
-    public function getFirstName()
-    {
-        return $this->first_name;
-    }
+    // public function setFirstName($fistName)
+    // {
+    //     $this->first_name = trim($fistName);
+    // }
 
-    public function setLastName($lastName)
-    {
-        $this->last_name =  trim($lastName);
-    }
+    // public function getFirstName()
+    // {
+    //     return $this->first_name;
+    // }
 
-    public function getLastName()
-    {
-        return $this->last_name;
-    }
+    // public function setLastName($lastName)
+    // {
+    //     $this->last_name =  trim($lastName);
+    // }
+
+    // public function getLastName()
+    // {
+    //     return $this->last_name;
+    // }
 
     public function getFullName()
     {
